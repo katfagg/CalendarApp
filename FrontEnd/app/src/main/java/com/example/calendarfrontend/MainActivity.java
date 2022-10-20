acd7452c006c893a8aaaaeb8dc5ba6b515ad4551
Kat Fagg <katjfagg@gmail.com>
2022-10-20 12:44:26 +0100

    sign in basic add

--- FrontEnd/app/src/main/java/com/example/calendarfrontend/MainActivity.java
+++ FrontEnd/app/src/main/java/com/example/calendarfrontend/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.calendarfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
