ed06e5be517e352faa5c6de561be43f4523fe482
Amina Salah <aminasalah56@outlook.com>
2022-10-21 15:35:07 +0100

    notes page

--- FrontEnd/app/src/main/java/com/example/calendarfrontend/MainActivity.java
+++ FrontEnd/app/src/main/java/com/example/calendarfrontend/MainActivity.java
@@ -1,21 +1,65 @@
 package com.example.calendarfrontend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+=======
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.HashSet;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< Updated upstream
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
 
@@ -28,7 +72,72 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
                 } else
                     Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
+=======
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        SharedPreferences sharedPreferences = getApplication().getSharedPreferences("com.notes", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null) {
+            notes.add("Example note");
+        } else {
+            notes = new ArrayList<>(set);
+        }
+
+        notes.add("Example note");
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+            }
+
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                int itemToDelete = i;
+
+                new AlertDialog.Builder(getApplicationContext())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                notes.remove(itemToDelete) ; //changed from 'i' to 'itemToDelete' so it doesn't call the above i
+                                arrayAdapter.notifyDataSetChanged();
+
+                                SharedPreferences sharedPreferences = getApplication().getSharedPreferences("com.notes", Context.MODE_PRIVATE);
+
+                                HashSet<String> set = new HashSet(MainActivity.notes);
+
+                                sharedPreferences.edit().putStringSet("notes", set).apply(); // set is now updated when a note is deleted
+                            }
+
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+>>>>>>> Stashed changes
             }
         });
     }
 }
+
+
+
+
+
+
