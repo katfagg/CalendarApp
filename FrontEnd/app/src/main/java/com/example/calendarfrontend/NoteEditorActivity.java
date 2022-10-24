18b7eecca2d66b9aceed3dd470fbee87d77f8abc
Amina Salah <aminasalah56@outlook.com>
2022-10-24 17:41:13 +0100

    compiler errors fix

--- FrontEnd/app/src/main/java/com/example/calendarfrontend/NoteEditorActivity.java
+++ FrontEnd/app/src/main/java/com/example/calendarfrontend/NoteEditorActivity.java
@@ -1,7 +1,5 @@
 package com.example.calendarfrontend;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
